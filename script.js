959cbca8626b3cea7f1b724d8d2a472fa588e708
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-29 23:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+for(let i =0; i<5;i++){
+	const delay = Math.floor(Math.random() * 5)+1;
+	const promise = new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(`promise ${i+1} resolved after ${delay} seconds`);
+		},delay*1000)
+	});
+	promises.push(promise)
+}
+Promise.any(promises)
+.then((result)=>{
+	document.getElementById('output').textContent = result;
+}).catch((err)=>{
+	console.log("all peomises are rejected", err);
+})
